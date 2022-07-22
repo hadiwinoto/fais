028ef9fa99046a473911fbee4a326223724d6e2d
hadiwinoto <inimulyohadiwinoto@gmail.com>
2022-07-22 14:40:44 +0700

    update

---
+++ resources/js/components/Dashboard.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <el-card class="box-card">
+                    <div slot="header" class="clearfix">
+                        <span>Dashboard</span>
+                        <el-button style="float: right; padding: 3px 0" type="text">Operation button</el-button>
+                    </div>
+                    <div class="text item">
+                        
+                    </div>
+                </el-card>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
